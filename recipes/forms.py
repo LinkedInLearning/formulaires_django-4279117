7b5c75f89b03c655caee6e98180b90fc4ac44a99
Labasse <labasse@gmail.com>
2025-01-14 22:28:15 +0000

    Sélectionner un widget (début)

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Recipe
 
+
 class SearchForm(forms.Form):
     query = forms.CharField(label='Nom ou ingrédient', required=False)
     vegan = forms.BooleanField(label='Végane', required=False)
@@ -10,11 +11,12 @@ class SearchForm(forms.Form):
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = ['title', 'vegan', 'category', 'quantity', 'description' ]
+        fields = ['title', 'vegan', 'category', 'quantity', 'description', 'instructions' ]
         labels = {
             'title': 'Nom',
             'vegan': 'Végane',
             'category': 'Catégorie',
             'quantity': 'Quantité',
             'description': 'Description',
+            'instructions': 'Instructions',
         }
